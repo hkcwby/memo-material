4cfaddf23fa51780b51047810287408e1c039eca
hkcwby <alexanderstevenson@me.com>
2023-04-26 00:28:20 -0500

    just playing around with Material UI after reading the documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import { useState } from "react";
 import "./App.css";
-import { Typography, Button } from "@mui/material";
+import { Container, Typography, Button, CssBaseline } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Typography variant="h1">Material UI Memo</Typography>
-      <div className="card">
-        <Button
-          variant="outlined"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <>
+      <CssBaseline />
+      <AppBar position="relative"></AppBar>
+      <Container className="App">
+        <Typography variant="h1">Material UI Memo</Typography>
+        <div className="card">
+          <Button
+            variant="outlined"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is {count}
+          </Button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      </Container>
+    </>
   );
 }
 
